5e89b118c2a04be1228f553cd48fad445fd5fcfd
Edmundas Rakauskas <rakauskased@gmail.com>
2018-04-03 17:46:49 +0300

    Added script to create gcp instance

--- config-scripts/create-reddit-vm.sh
+++ config-scripts/create-reddit-vm.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-image=$(gcloud compute images list | grep reddit-full| sort -r| head -1| cut -f 1 -d ' ')
 
-
-gcloud compute instances create reddit-app --boot-disk-size=10GB --image-family ubuntu-1604-lts --machine-type=g1-small --tags puma-server --restart-on-failure --metadata-from-file  startup-script='./startup_script.sh'
+gcloud compute instances create reddit-app --boot-disk-size=10GB --image-family reddit-full --machine-type=g1-small --tags puma-server --restart-on-failure
 
 
 
