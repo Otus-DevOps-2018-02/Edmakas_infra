4137b5afd80de80a03bc76ac7cfd5cdc59959e66
Edmundas Rakauskas <rakauskased@gmail.com>
2018-04-18 17:57:08 +0300

    Ansible-2. Added *.retry to gitignore. Removed unnecessary variables from terraform/variables.tf (its added into modules)

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,17 +15,3 @@ variable zone {
 variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
-
-variable private_key_path_conn {
-  description = "Path to the private key used by provisioner connection"
-}
-
-variable app_disk_image {
-  description = "Disk image for reddit app"
-  default     = "reddit-app-base"
-}
-
-variable db_disk_image {
-  description = "Disk image with mongodb for reddit"
-  default     = "reddit-db-base"
-}
